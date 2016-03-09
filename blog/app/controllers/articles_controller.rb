545760ef17d38a2a34456cdce352644c9a07f53c
Azucena Reyes <zucisita@gmail.com>
2016-03-08 22:21:53 -0600

    add action destroy to Articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -21,6 +21,12 @@ class ArticlesController < ApplicationController
    end
  end
 
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  redirect_to articles_path
+end
+
  def update
 
  end
