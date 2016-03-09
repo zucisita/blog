1f25b5b69c3342f2fa6c4ab252fa1a6eecd7123c
Azucena Reyes <zucisita@gmail.com>
2016-03-08 20:19:15 -0600

    add the actions "new, create" to the article controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -7,4 +7,18 @@ class ArticlesController < ApplicationController
  def show
    @article = Article.find(params[:id])
  end
+
+ def new
+   @article = Article.new
+ end
+
+ def create
+   @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+   @article.save
+   redirect_to @article
+ end
+
+ def update
+
+ end
 end
