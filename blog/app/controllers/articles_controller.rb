57ac517e6f7fddb6b7ac416d7816b76d22291b6f
Azucena Reyes <zucisita@gmail.com>
2016-03-10 13:20:25 -0600

    configure action:edit in article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -12,8 +12,12 @@ class ArticlesController < ApplicationController
    @article = Article.new
  end
 
+ def edit
+   @article = Article.find(params[:id])
+ end
+
  def create
-   @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+   @article = Article.new(article_params)
    if @article.save
      redirect_to @article
    else
@@ -28,6 +32,17 @@ def destroy
 end
 
  def update
+   @article = Article.find(params[:id])
+   if @article.update(article_params)
+     redirect_to @article
+   else
+     render :edit
+   end
+ end
 
+ private
+
+ def article_params
+   params.require(:article).permit(:title,:body)
  end
 end
