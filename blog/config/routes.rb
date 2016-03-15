0bdcc15375e07d17d75bedd8b209b005c0bfda37
Azucena Reyes <zucisita@gmail.com>
2016-03-15 17:58:20 -0600

    enable json.jbuilder in comment

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :articles do
-    resources :comments, only: [:create, :destroy, :update]
+    resources :comments, only: [:create, :destroy, :update, :show]
   end
   devise_for :users
    root 'welcome#index'
