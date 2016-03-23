2decf14931c799243252fa68c1891b6b418ca951
Azucena Reyes <zucisita@gmail.com>
2016-03-23 16:21:41 -0600

    add avatar to comments into view Article

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -5,5 +5,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles
   has_many :comments
-  include PermissionsConcern 
+  include PermissionsConcern
+
+  def avatar
+    email_address = self.email.downcase
+
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    image_src = "http://www.gravatar.com/avatar/#{hash}"
+  end
 end
