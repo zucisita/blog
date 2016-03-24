21bc6f008a81152b11da7d59d0093026c48ec96e
Azucena Reyes <zucisita@gmail.com>
2016-03-23 18:58:56 -0600

    configure cover in each Article

--- blog/app/helpers/application_helper.rb
+++ blog/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def article_cover url, options ={}
+    html_class = options[:class]
+    html_style = "background:url(#{url});"\
+                        "width:100%;height:400px;background-size:cover;"
+
+    html = "<header style='#{html_style}' class='#{html_class}'>"\
+              "</header>"
+    html.html_safe
+  end
 end
