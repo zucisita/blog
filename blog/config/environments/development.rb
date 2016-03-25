eb70345a3578ace07ac291d503ad05bba2b07fd6
Azucena Reyes <zucisita@gmail.com>
2016-03-24 19:24:38 -0600

    configure action_mailer

--- blog/config/environments/development.rb
+++ blog/config/environments/development.rb
@@ -19,6 +19,10 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  #For mailing
+  config.action_mailer.default_url_options = { host: "localhost" }
+  config.action_mailer.delivery_method = :letter_opener
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
